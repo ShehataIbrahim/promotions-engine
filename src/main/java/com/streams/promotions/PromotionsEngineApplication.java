27a4199319207513e9d5452d916ed5621d3972d2
Shehata Ibrahim <eng.shehata.ibrahim@gmail.com>
2023-12-17 14:39:32 +0100

    Initial Commit

---
+++ src/main/java/com/streams/promotions/PromotionsEngineApplication.java
@@ -0,0 +1,13 @@
+package com.streams.promotions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionsEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionsEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/streams/promotions/PromotionsEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streams.promotions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionsEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
